660e538e5694d42c6f9030329e60eae72a2e62e9
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-05-03 11:40:59 -0700

    maybe removed trailing slash requirements

--- scotland_yard/scotland_yard/settings.py
+++ scotland_yard/scotland_yard/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+APPEND_SLASH = False
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
