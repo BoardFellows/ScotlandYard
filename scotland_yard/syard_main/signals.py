4de90e5364d3af1fc933c03beb7257df0034b5c5
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-05-03 11:02:57 -0700

    #13 added basic signal for creating an auth token

---
+++ scotland_yard/syard_main/signals.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+for user in User.objects.all():
+    Token.objects.get_or_create(user=user)
