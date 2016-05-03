82a96c18833dbc20dec8bbf22cc2b27becaf7d19
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-05-03 11:05:52 -0700

    #13 import signals on ready

--- scotland_yard/syard_main/apps.py
+++ scotland_yard/syard_main/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
 
-
 class SyardMainConfig(AppConfig):
+    """Config Syard Main."""
+
     name = 'syard_main'
+
+    def ready(self):
+        """Import signals on ready."""
+        from syard_main import signals
