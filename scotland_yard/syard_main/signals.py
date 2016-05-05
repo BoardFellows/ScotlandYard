97731a5bbfda443df895e3a354a6554d7c8d73d6
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-05-05 13:45:54 -0700

    combining all signals into one file?

--- scotland_yard/syard_main/signals.py
+++ scotland_yard/syard_main/signals.py
@@ -5,7 +5,3 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
