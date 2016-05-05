a837ab051c8301b2c795b17b2798784a68f0b81a
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-05-05 13:46:07 -0700

    delete redundant signal file

--- scotland_yard/syard_main/signals.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-
-
